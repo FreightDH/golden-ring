7fcdbd29aa48abef66f06da867235211afbb5179
FreightDH <maxim.zolotkov@yandex.ru>
2023-07-09 00:56:00 +0300

    feat: add automatic generation of slides

--- golden-ring/src/js/app.js
+++ golden-ring/src/js/app.js
@@ -1,8 +1,9 @@
 import '../scss/style.scss';
 import * as myFunctions from './files/functions.js';
 import * as menuFunctions from './files/burger.js';
-
+import { generateSlides } from './files/slider.js';
 // Проверка поддержки webp, добавление класса webp или no-webp для HTML
 myFunctions.isWebp();
+generateSlides();
 // Модуль для работы с меню-бургер
 // menuFunctions.menuInit();
