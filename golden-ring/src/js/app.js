3b88d95abe347dd23af411ef4dc495dccb44c8ca
FreightDH <maxim.zolotkov@yandex.ru>
2023-07-09 14:16:50 +0300

    feat: import swiper to app.js

--- golden-ring/src/js/app.js
+++ golden-ring/src/js/app.js
@@ -1,9 +1,11 @@
 import '../scss/style.scss';
 import * as myFunctions from './files/functions.js';
 import * as menuFunctions from './files/burger.js';
-import { generateSlides } from './files/slider.js';
+import { generateSlides } from './files/slides.js';
+import { swiperInit } from './files/swiper.js';
 // Проверка поддержки webp, добавление класса webp или no-webp для HTML
 myFunctions.isWebp();
-generateSlides();
 // Модуль для работы с меню-бургер
 // menuFunctions.menuInit();
+generateSlides();
+swiperInit();
