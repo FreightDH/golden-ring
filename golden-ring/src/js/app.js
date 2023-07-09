c716e1f25132383f5e5f7e2ee0aa97ac6a3def54
FreightDH <maxim.zolotkov@yandex.ru>
2023-07-09 17:41:34 +0300

    refactor: create directory for slider scripts, change imports

--- golden-ring/src/js/app.js
+++ golden-ring/src/js/app.js
@@ -1,8 +1,8 @@
 import '../scss/style.scss';
 import * as myFunctions from './files/functions.js';
 import * as menuFunctions from './files/burger.js';
-import { generateSlides } from './files/slides.js';
-import { swiperInit } from './files/swiper.js';
+import { generateSlides } from './files/slider/slides.js';
+import { swiperInit } from './files/slider/swiper.js';
 // Проверка поддержки webp, добавление класса webp или no-webp для HTML
 myFunctions.isWebp();
 // Модуль для работы с меню-бургер
