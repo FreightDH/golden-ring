cc4f9d6c1e2f918d67b7374f7e81d16d2730334f
FreightDH <maxim.zolotkov@yandex.ru>
2023-07-11 21:43:23 +0300

    refactor: app.js

--- golden-ring/src/js/app.js
+++ golden-ring/src/js/app.js
@@ -1,11 +1,11 @@
 import '../scss/style.scss';
 import * as myFunctions from './files/functions.js';
-import * as menuFunctions from './files/burger.js';
 import { generateSlides } from './files/slider/slides.js';
 import { swiperInit } from './files/slider/swiper.js';
+
 // Проверка поддержки webp, добавление класса webp или no-webp для HTML
 myFunctions.isWebp();
-// Модуль для работы с меню-бургер
-// menuFunctions.menuInit();
+
+// Основные функции
 generateSlides();
 swiperInit();
